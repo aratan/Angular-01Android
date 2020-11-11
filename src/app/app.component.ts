3c42f292d83da9626084b7018415d83eeb4161b5
Víctor <systemdeveloper868@gmail.com>
2020-11-11 21:44:28 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,20 @@ export class AppComponent implements OnInit {
   show:boolean = false;
   n:number = 0;
   arr:string[] = ["Hola","adios","despues","repite"];
-
+  datos =  this._ServicioTask.getDato().subscribe()
+  
   constructor(private _ServicioTask: ServicioTask){
 
   }
 
+
+
   ngOnInit(){
     this._ServicioTask.getDato().subscribe(
-      data => console.log(data),
+      data => console.log(data[0].name),
       err => console.log(err),
       () => console.log('Peticion finalizada'))
+      
   }
   
 }
